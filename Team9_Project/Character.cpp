0ea3435ac776cc68c64fd40f238b3f0db6108dc9
afsd3330-dotcom <afsd3330@gmail.com>
2025-12-30 20:08:12 +0900

    Character
    캐릭터의 기본적인 이름 체력 최대체력 공격력 경험치 경험치통 레벨 골드 인벤토리칸 개수 정도만 간단하게 구현을 해놓은 상태

--- Team9_Project/Character.cpp
+++ Team9_Project/Character.cpp
@@ -1,2 +1,40 @@
-﻿
-#include "Character.h"
+﻿#include "Character.h"
+
+using namespace std;
+
+Character::Character(string name, int maxHp, int atk)
+	:Name(name), HP(200), MaxHP(maxHp), ATK(atk), EXP(0), Level(1), Gold(0), EXPToLevelUp(100), MaxLevel(10),MaxInventorySize(20) {
+}
+string Character::getName()const { return Name; }
+int Character::getHP()const { return HP; }
+int Character::getMaxHP()const { return MaxHP; }
+int Character::getATK()const { return ATK; }
+int Character::getEXP()const { return EXP; }
+int Character::getEXPToLevelUp()const { return EXPToLevelUp; }
+int Character::getLevel()const { return Level; }
+int Character::getGold()const { return Gold; }
+vector<Item> Character::getInventory()const { return Inventory; }
+
+void Character::setHP(int hp) { HP = hp; }
+void Character::setMaxHP(int maxHp) { MaxHP = maxHp; }
+void Character::setATK(int atk) { ATK = atk; }
+void Character::setEXP(int exp) { EXP = exp; }
+void Character::setLevel(int level) { Level = level; }
+void Character::setGold(int gold) { Gold = gold; }
+
+void Character::LevelUp() {
+	if (Level < MaxLevel && EXP >= EXPToLevelUp) {
+		Level++;
+		MaxHP += 60;
+		ATK += 30;
+		EXP = 0;
+		EXPToLevelUp = 100;
+	}
+}
+void Character::Attack(Character& target) {
+	target.GetHit(ATK);
+}
+void Character::GetHit(int damage) {
+	HP -= damage;
+	if (HP < 0) HP = 0;
+}
--- Team9_Project/Character.h
+++ Team9_Project/Character.h
@@ -1,5 +1,50 @@
 ﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Item.h"
+
+using namespace std;
+
 class Character
 {
+private:
+	string Name;
+	int HP, MaxHP;
+	int ATK;
+	int EXP;
+	int EXPToLevelUp;
+	int Level;
+	int MaxLevel;
+	int Gold;
+	int MaxInventorySize;
+
+	vector<Item> Inventory;
+
+
+public:
+	Character(string name, int maxHp, int atk);
+
+	string getName()const;
+	int getHP()const;
+	int getMaxHP()const;
+	int getATK()const;
+	int getEXP()const;
+	int getLevel()const;
+	int getEXPToLevelUp()const;
+	int getGold()const;
+	vector<Item> getInventory()const;
+
+	void setHP(int Hp);
+	void setMaxHP(int MaxHp);
+	void setATK(int atk);
+	void setEXP(int exp);
+	void setLevel(int level);
+	void setGold(int gold);
+
+	void LevelUp();
+	void Attack(Character& target);
+	void GetHit(int damage);
+
 };
 
