c985760343ca20a034689ccf79b76b328fb52502
honghardy-sole <hongtop1124@gmail.com>
2025-12-30 19:36:19 +0900

    MidBoss_Basic Code(1)
    MidBoss 코드입니다.
    **Monster Class 상속 받아야함**

--- Team9_Project/MidBoss.h
+++ Team9_Project/MidBoss.h
@@ -1,7 +1,19 @@
-﻿#pragma once
+﻿// MidBoss.h
+#pragma once
 #include "Monster.h"
-class MidBoss :
-    public Monster
-{
-};
+#include "MidBossData.h"
+#include <map>
 
+class MidBoss : public Monster //MidBoss는 Monster 상속
+{
+private:
+    int dropItemId; // CashableItems 대체 (ID로 관리)
+
+    static std::map<int, MidBossData> midBossTable;
+
+public:
+    MidBoss(int id);
+
+    void Attack() override;
+    void GetHit(int damage) override;
+};
